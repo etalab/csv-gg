a349ed24cde867fb5d73ac239ea38478650f9cc3
Alexandre Bulté <alexandre@bulte.net>
2019-04-10 10:08:26 +0200

    Remove build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-. "/usr/local/opt/nvm/nvm.sh" && nvm use
-npm run build-production
-rm -rf docs
-mv dist docs
-echo forms.validata.etalab.studio > docs/CNAME
-git add docs
-git commit docs -m 'auto-build vue app'
