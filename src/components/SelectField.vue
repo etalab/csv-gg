0fa2aad31b5ba3fdf61aa41e599656ff895b3820
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-06-26 11:23:21 +0200

    Switch to mixins

--- src/components/SelectField.vue
+++ src/components/SelectField.vue
@@ -15,52 +15,14 @@
     </div>
 </template>
 <script>
-import { EventBus } from '@/event-bus.js';
+import ValidateField from '@/mixins/ValidateField.vue'
 
 export default {
     name: 'SelectField',
-    props: {
-        field: Object,
-    },
-    data() {
-        return {
-            error: false,
-            formValidated: false,
-            value: null,
-        }
-    },
-    mounted() {
-        EventBus.$on('field-error', (field_name, error) => {
-            if (this.field.name !== field_name) return
-            this.error = error
-        })
-        EventBus.$on('field-no-error', (field_name) => {
-            if (this.field.name !== field_name) return
-            this.error = false
-        })
-        EventBus.$on('form-validated', () => {
-            this.formValidated = true
-        })
-        EventBus.$on('form-reset', () => {
-            this.formValidated = false
-            this.error = false
-            this.value = null
-        })
-    },
+    mixins: [ValidateField],
     computed: {
-        isValid() {
-            if (!this.formValidated) return null
-            return !this.error
-        },
         options() {
             return this.field.constraints.enum
-        }
-    },
-    methods: {
-        onInput(event) {
-            this.formValidated = false
-            this.error = false
-            EventBus.$emit('field-value-changed', this.field.name, event)
         }
     }
 }
--- src/components/StringField.vue
+++ src/components/StringField.vue
@@ -15,50 +15,10 @@
     </div>
 </template>
 <script>
-import { EventBus } from '@/event-bus.js';
+import ValidateField from '@/mixins/ValidateField.vue'
 
 export default {
     name: 'StringField',
-    props: {
-        field: Object,
-    },
-    data() {
-        return {
-            error: false,
-            formValidated: false,
-            value: null,
-        }
-    },
-    mounted() {
-        EventBus.$on('field-error', (field_name, error) => {
-            if (this.field.name !== field_name) return
-            this.error = error
-        })
-        EventBus.$on('field-no-error', (field_name) => {
-            if (this.field.name !== field_name) return
-            this.error = false
-        })
-        EventBus.$on('form-validated', () => {
-            this.formValidated = true
-        })
-        EventBus.$on('form-reset', () => {
-            this.formValidated = false
-            this.error = false
-            this.value = null
-        })
-    },
-    computed: {
-        isValid() {
-            if (!this.formValidated) return null
-            return !this.error
-        },
-    },
-    methods: {
-        onInput(event) {
-            this.formValidated = false
-            this.error = false
-            EventBus.$emit('field-value-changed', this.field.name, event)
-        }
-    }
+    mixins: [ValidateField]
 }
 </script>
