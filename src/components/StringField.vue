7d4f89b610b2a6b8a4427e56ec7dfb0ec10a82b7
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-06-27 10:59:55 +0200

    Extra constraints for StringField

--- src/components/StringField.vue
+++ src/components/StringField.vue
@@ -10,7 +10,18 @@
             valid-feedback="Ce champ est valide"
             :state="isValid"
           >
-              <b-form-input v-model="value" :id="`field-${field.name}`" :placeholder="field.example" v-on:input="onInput" :state="isValid" trim/>
+            <b-form-input
+                v-model="value"
+                :id="`field-${field.name}`"
+                :type="type"
+                :placeholder="field.example"
+                :required="field.constraints ? field.constraints.required : false"
+                :min="field.constraints ? field.constraints.minimum : null"
+                :max="field.constraints ? field.constraints.maximum : null"
+                v-on:input="onInput"
+                :state="isValid"
+                trim="true"
+            />
         </b-form-group>
     </div>
 </template>
@@ -19,6 +30,17 @@ import ValidateField from '@/mixins/ValidateField.vue'
 
 export default {
     name: 'StringField',
-    mixins: [ValidateField]
+    mixins: [ValidateField],
+    computed: {
+        type() {
+            if (['number', 'float'].includes(this.field.type)) {
+                return 'number'
+            } else if (this.field.format && this.field.format === 'email') {
+                return 'email'
+            }
+
+            return 'text'
+        }
+    }
 }
 </script>
