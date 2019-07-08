5ef4a6fa69ebfe9670e7c485999243e705345e3f
Alexandre Bulté <alexandre@bulte.net>
2019-07-08 13:38:24 +0200

    Add warning and credits

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
       <b-container>
           <h1 class="text-center mt-2"><img src="./assets/csvgg.gif" alt="csv-gg"></h1>
           <router-view/>
+          <footer class="text-center">
+              <hr/>
+              <p class="mb-0">
+                  <strong>csv-gg</strong> est un <a href="https://github.com/etalab/csv-gg">logiciel libre</a> développé par <a href="https://etalab.gouv.fr/">Etalab</a>.
+              </p>
+              <p class="mt-0">
+                  <span class="badge badge-pill badge-warning">Attention</span> il s'agit d'un projet expérimental. En cas de question ou de problème, vous pouvez <a href="https://github.com/etalab/csv-gg/issues/new">ouvrir un ticket ici</a>.
+              </p>
+          </footer>
       </b-container>
   </div>
 </template>
