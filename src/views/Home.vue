c891aa6eda88b21839a1c547f8bf822fa0236f44
Alexandre Bulté <alexandre@bulte.net>
2019-07-08 16:31:19 +0200

    fix typography (#18)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,10 @@
             <h4 class="alert-heading">Mode d'emploi</h4>
             <p>Cet outil vous permet de créer un fichier CSV en vous assurant qu'il est conforme à un schéma, c'est-à-dire que ses données sont complètes, valides et structurées.</p>
             <ol class="mb-0">
-                <li>Sélectionnez le schéma qui vous intéresse dans la liste déroulante, les schémas disponibles ici étant ceux référencés sur <a href="https://schema.data.gouv.fr">schema.data.gouv.fr</a>&nbsp;;</li>
-                <li>Remplissez le formulaire&nbsp;: vous allez ainsi créer la première ligne de votre fichier CSV&nbsp;;</li>
-                <li>L'outil vous prévient d'éventuelles erreurs de validation, le cas échéant vous pouvez les corriger&nbsp;;</li>
-                <li>Une fois votre formulaire valide, les valeurs apparaissent sous la forme d'une ligne dans un tableau récapitulatif&nbsp;;</li>
+                <li>Sélectionnez le schéma qui vous intéresse dans la liste déroulante, les schémas disponibles ici étant ceux référencés sur <a href="https://schema.data.gouv.fr">schema.data.gouv.fr</a>.</li>
+                <li>Remplissez le formulaire&nbsp;: vous allez ainsi créer la première ligne de votre fichier CSV.</li>
+                <li>L'outil vous prévient d'éventuelles erreurs de validation, le cas échéant vous pouvez les corriger.</li>
+                <li>Une fois votre formulaire valide, les valeurs apparaissent sous la forme d'une ligne dans un tableau récapitulatif.</li>
                 <li>Vous pouvez alors choisir d'ajouter une ou plusieurs lignes (répétez les étapes 2 à 4) ou télécharger le fichier CSV correspondant au tableau récapitulatif.</li>
             </ol>
         </b-alert>
