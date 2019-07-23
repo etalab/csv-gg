32e7aa98c8ea29471d5b0aff9726443f300899db
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-07-23 10:48:50 +0200

    Add INSEE hint

--- src/components/StringField.vue
+++ src/components/StringField.vue
@@ -22,15 +22,43 @@
                 :state="isValid"
                 :trim="true"
             />
+            <div class="mt-2" v-if="city">Ce code INSEE correspond à {{ this.city }}.</div>
         </b-form-group>
     </div>
 </template>
 <script>
+import { EventBus } from '@/event-bus.js'
 import ValidateField from '@/mixins/ValidateField.vue'
 
 export default {
     name: 'StringField',
     mixins: [ValidateField],
+    data() {
+        return {
+            city: null
+        }
+    },
+    mounted() {
+        EventBus.$on('field-value-changed', (field_name, value) => {
+            if (this.field.name !== field_name) return
+            if (this.isInsee) {
+                this.handleInseeInput(value)
+            }
+        })
+    },
+    methods: {
+        handleInseeInput: function(value) {
+            fetch(`https://geo.api.gouv.fr/communes/${value}`).then(r => {
+                if (!r.ok) {
+                    this.city = null
+                    throw new Error("Not 200 response")
+                }
+                return r.json()
+            }).then(data => {
+                this.city = data.nom
+            })
+        }
+    },
     computed: {
         type() {
             if (['number', 'float'].includes(this.field.type)) {
@@ -40,6 +68,9 @@ export default {
             }
 
             return 'text'
+        },
+        isInsee() {
+            return this.field.name.toLowerCase().includes('insee')
         }
     }
 }
