e10f50627f38b6a53c7c76cac7d5df0cea3a77a6
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-07-23 15:36:59 +0200

    Add SIRET hint (#27)

--- src/components/StringField.vue
+++ src/components/StringField.vue
@@ -22,8 +22,14 @@
                 :state="isValid"
                 :trim="true"
             />
+
+            <!-- INSEE -->
             <div class="mt-2 valid-feedback d-block" v-if="isInsee && city">Ce code INSEE correspond à {{ this.city }}.</div>
             <div class="mt-2 invalid-feedback d-block" v-if="isInsee && !city && value">Ce code INSEE n'existe pas.</div>
+
+            <!-- SIRET -->
+            <div class="mt-2 valid-feedback d-block" v-if="isSiret && siretDescription">Ce SIRET correspond à {{ this.siretDescription }}.</div>
+            <div class="mt-2 invalid-feedback d-block" v-if="isSiret && !siretDescription && value">Ce code SIRET n'existe pas.</div>
         </b-form-group>
     </div>
 </template>
@@ -36,14 +42,18 @@ export default {
     mixins: [ValidateField],
     data() {
         return {
-            city: null
+            city: null,
+            siretDescription: null
         }
     },
     mounted() {
         EventBus.$on('field-value-changed', (field_name, value) => {
             if (this.field.name !== field_name) return
+
             if (this.isInsee) {
                 this.handleInseeInput(value)
+            } else if (this.isSiret) {
+                this.handleSiretInput(value)
             }
         })
     },
@@ -57,6 +67,22 @@ export default {
                 return r.json()
             }).then(data => {
                 this.city = data.nom
+            })
+            .catch(_ => _)
+        },
+        handleSiretInput: function(value) {
+            this.siretDescription = null
+            if (value.length !== 14) return
+
+            fetch(`https://entreprise.data.gouv.fr/api/sirene/v1/siret/${value}`).then(r => {
+                if (!r.ok) {
+                    this.siretDescription = null
+                    throw new Error("Not 200 response")
+                }
+                return r.json()
+            }).then(data => {
+                const company = data.etablissement
+                this.siretDescription = `${company.nom_raison_sociale} (${company.libelle_activite_principale})`
             })
             .catch(_ => _)
         }
@@ -73,6 +99,9 @@ export default {
         },
         isInsee() {
             return this.field.name.toLowerCase().includes('insee')
+        },
+        isSiret() {
+            return this.field.name.toLowerCase().includes('siret')
         }
     }
 }
