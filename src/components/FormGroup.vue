4bc275412dc3f86a830aeb7e1a354d9a88eddb6d
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-12-24 11:04:45 +0100

    Extract a FormGroup component

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-form-group
+        label-cols-sm="4"
+        label-cols-lg="3"
+        :label="field.name"
+        :label-for="`field-${field.name}`"
+        :invalid-feedback="error.content"
+        valid-feedback="Ce champ est valide"
+        :state="isValid"
+        :label-class="{'required': isRequired}"
+    >
+        <template slot="description">
+            <vue-markdown :source="field.description" />
+        </template>
+
+        <slot></slot>
+
+    </b-form-group>
+</template>
+<script>
+import VueMarkdown from 'vue-markdown'
+import ValidateField from '@/mixins/ValidateField.vue'
+
+export default {
+    name: 'FormGroup',
+    mixins: [ValidateField],
+    components: {
+        VueMarkdown
+    }
+}
+</script>
