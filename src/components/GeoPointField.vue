9977a252d4c31eb95da0c4d31088a689558b5584
Pierre Dittgen <pierre.dittgen@jailbreak.paris>
2020-12-10 18:10:46 +0100

    wip

--- src/components/GeoPointField.vue
+++ src/components/GeoPointField.vue
@@ -1,6 +1,6 @@
 <template>
   <FormGroup :field="field" :error="error">
-    <b-form-input
+    <!-- <b-form-input
       v-model="value"
       :id="`field-${field.name}`"
       type="text"
@@ -8,18 +8,68 @@
       :required="isRequired"
       v-on:input="onInput"
       :state="isValid"
-    />
+    /> -->
+    <div id="my_map">
+      <l-map ref="myMap"></l-map>
+    </div>
   </FormGroup>
 </template>
+
 <script>
 import FormGroup from '@/components/FormGroup.vue'
 import ValidateField from '@/mixins/ValidateField.vue'
+import L from 'leaflet'
+
+
+function initMap(map) {
+  // Initial settings
+  const coords = [46, 2]
+  const zoomLevel = 12
+
+  map.setView(coords, zoomLevel)
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  let marker = L.marker(coords, {
+    draggable: true
+  }).addTo(map)
+    .bindPopup('Choisir<br/>un point géographique')
+    .openPopup();
+
+  marker.on('dragstart', function(e) {
+    marker.closePopup()
+  })
+  marker.on('dragend', function(e) {
+    const lon = marker.getLatLng().lng
+    const lat = marker.getLatLng().lat
+    marker.bindPopup(`${lon.toFixed(2)}, ${lat.toFixed(2)}`)
+    marker.openPopup()
+  })
+
+}
 
 export default {
   name: 'GeoPointField',
   mixins: [ValidateField],
   components: {
     FormGroup
-  }
+  },
+  data() {
+    return {
+      lon: null,
+      lat: null,
+    }
+  },
+  mounted() {
+      const map = this.$refs.myMap.mapObject
+      initMap(map)
+  },
 }
 </script>
+
+<style>
+  #my_map {
+    height: 300px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,14 +3,29 @@ import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import Loading from 'vue-loading-overlay'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import { Icon } from 'leaflet';
 
 import 'vue-loading-overlay/dist/vue-loading.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'leaflet/dist/leaflet.css';
+
+// https://vue2-leaflet.netlify.app/quickstart/#marker-icons-are-missing
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(Loading)
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
 
 new Vue({
   router,
