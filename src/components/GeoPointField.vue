e709418f42ae5ec5b4604bf6dba99c219e300268
Pierre Dittgen <pierre.dittgen@jailbreak.paris>
2020-12-21 16:08:38 +0100

    wip - work two-way

--- src/components/GeoPointField.vue
+++ src/components/GeoPointField.vue
@@ -13,8 +13,16 @@
             :trim="true"
         /> -->
     <div style="display: grid; grid-template-columns: auto 60px;">
-      <b-form-input />
-      <button @click.prevent="showMap = !showMap">
+      <b-form-input
+        v-model="value"
+        :id="`field-${field.name}`"
+        :placeholder="field.example"
+        :required="isRequired"
+        v-on:blur="onBlur"
+        :state="isValid"
+        :trim="true"
+      />
+      <button v-on:click.prevent="showMap = !showMap">
           GEO
       </button>
     </div>
@@ -32,7 +40,7 @@
         <l-marker
           :lat-lng="markerLocation"
           :draggable="true"
-          @update:latLng="updateLocation"
+          @update:latLng="onMarkerMove"
           >
           <l-tooltip>
             Déplacez le marqueur<br/>
@@ -61,26 +69,69 @@ export default {
     LTooltip
   },
   methods: {
-    updateLocation(center) {
-      this.markerLocation = center
+    onBlur() {
+      this.onInput(this.value)
+    },
+    onInput(strValue) {
+      const latLng = this.extractLatLng(strValue)
+      const valid = strValue == '' || latLng !== false
+      EventBus.$emit('field-error', this.field.name, !valid)
+      EventBus.$emit('field-value-changed', this.field.name, strValue)
 
+      if (latLng !== false) {
+        this.recenterMap(latLng)
+      }
+    },
+    formatLatLng(lngLat) {
       const fp = this.floatPrecision
-      const value = `${center.lng.toFixed(fp)}, ${center.lat.toFixed(fp)}`
+      const { lat, lng } = lngLat
+      return `${lng.toFixed(fp)}, ${lat.toFixed(fp)}`
+    },
+    extractLatLng(str) {
+      if (str == '') {
+        return false
+      }
+      const chunks = str.split(',').map((elt) => elt.trim())
+
+      if (chunks.length != 2) {
+        return false
+      }
+      const lon = parseFloat(chunks[0])
+      const lat = parseFloat(chunks[1])
+      if (isNaN(lon) || isNaN(lat)) {
+        return false;
+      }
+      // TODO: check lon and lat bounds
+      return { lon, lat }
+    },
+    onMarkerMove(latLng) {
+
+      // marker location
+      this.markerLocation = latLng
+
+      // formatted value: "lon, lat"
+      const value = this.formatLatLng(latLng)
+
+      // update text input
+      this.value = value
+
+      // Hey! value has changed
       EventBus.$emit('field-value-changed', this.field.name, value)
     },
-    limitPrecision(floatVal) {
-      return floatVal.toFixed(this.floatPrecision)
+    recenterMap(latLng) {
+      this.markerLocation = latLng
+      this.center = latLng
     }
   },
   data() {
     return {
-      center: [46, 2],
-      zoom: 12,
+      center: [46.8, 2.11],
+      zoom: 5,
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       markerLocation: [46, 2],
-      values: [[46, 2]],
-      floatPrecision: 3,
+      value: '',
+      floatPrecision: 2,
       showMap: false,
     }
   },
