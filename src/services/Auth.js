8253242735c9a8363266b1be1ee9573f837e0253
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-02-11 09:56:35 +0100

    .env .env.secret

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -33,6 +33,7 @@ export default class Auth {
         const encodedState = encodeURIComponent(state)
         const encodedCC = encodeURIComponent(codeChallenge)
 
+            
         return `${this.BASE_URL}/oauth/authorize?redirect_uri=${redirectURI}&response_type=code&state=${encodedState}&client_id=${this.clientId}&scope=default&code_challenge=${encodedCC}&code_challenge_method=S256`
     }
 
